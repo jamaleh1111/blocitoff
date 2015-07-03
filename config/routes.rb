cd5d50af1168d0b01421d68e35c86b9833227a50
Jamaleh <Jamaleh@hotmail.com>
2015-07-03 19:29:32 +1000

    progress on user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,23 @@
 Rails.application.routes.draw do
+
   get 'welcome/home'
 
   get 'welcome/index'
 
 
   devise_for :users
+  resources :users, only: [:show]
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root to: 'welcome#home'
+
+  
+end
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -60,4 +67,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
