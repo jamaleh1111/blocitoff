240084d679a99bd5d92e084635fbe9758392ecec
Jamaleh <Jamaleh@hotmail.com>
2015-07-05 02:53:24 +1000

    seeded users and items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,26 @@
 require 'faker'
 
+5.times do 
+  user = User.new(
+    name:      Faker::Name.name,
+    email:     Faker::Internet.email,
+    password:  Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
+  
+50.times do 
+  items = Item.create!(
+    user:    users.sample,
+    name:    Faker::Lorem.sentence
+  )
+end 
+items = Item.all
+
+puts "Seed finished"
 #
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
