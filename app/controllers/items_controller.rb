b9d7382705ef3cfa07bcdd79c90e2f26d2ece2bf
Jamaleh <Jamaleh@hotmail.com>
2015-07-04 12:49:12 +1000

    work on todo items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,14 @@
 class ItemsController < ApplicationController
   def create
-    @item = Item.new(params[:id])
-    @item.user = current_user
+    @item = Item.new(params.require(:item).permit(:description))
+    @item.current_user = @item
 
     if @item.save 
       flash[:notice] = "Item was saved"
-      redirect_to @item
+      redirect_to user_path(current_user)
     else
       flash[:error] = "There was an error saving your item, please try again."
-      redirect_to @item
+      redirect_to user_path(current_user)
     end
-
   end
 end
