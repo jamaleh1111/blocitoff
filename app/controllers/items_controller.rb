7094f63e97d5b848a27694d8283c67f8fb7a9738
Jamaleh <Jamaleh@hotmail.com>
2015-07-07 11:13:49 +1000

    destroy items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,39 +2,39 @@ class ItemsController < ApplicationController
 
 
   def create
-    @user = User.find(params[:user_id])
-    @item = @user.items
     @item = current_user.items.build(item_params)
-    @item.user = current_user
-    @item.user = @user
-    @new_item = Item.new
+    #@new_item = Item.new
     
     if @item.save 
       flash[:notice] = "Item was saved"
+      redirect_to current_user
     else
       flash[:error] = "There was an error saving your item, please try again."
+      redirect_to current_user
     end
 
-    respond_to do |format|
-      format.html
-      format.js
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.js
+    # end
   end
 
   def destroy
-    @user = User.find(params[:user_id])
-    @item = @user.items.find(params[:id])
     
+    @item = current_user.items.find(params[:id])
+
     if @item.destroy
       flash[:notice] = "Item is completed!"
+      redirect_to current_user
     else
       flash[:error] = "Sorry, there was a problem, please try again."
+      redirect_to current_user
     end  
 
-    respond_to do |format|
-      format.html
-      format.js
-    end 
+    # respond_to do |format|
+    #   format.html
+    #   format.js
+    # end 
   end
 
   def item_params
